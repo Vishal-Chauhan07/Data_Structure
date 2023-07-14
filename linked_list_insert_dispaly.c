6eacfbdfee6f0022fdd459d7a474d78bf68fa188
Vishal Rambilash Chauhan <138654751+Vishal-Chauhan07@users.noreply.github.com>
2023-07-14 11:00:47 +0530

    Add files via upload

--- linked_list_insert_dispaly.c
+++ linked_list_insert_dispaly.c
@@ -1,39 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node{
-	int data;
-	struct node *next;
-};
-struct node *start=NULL,*newnode,*cur,*ptr;
-void create()
-{
-	newnode=(struct node*)malloc(sizeof(struct node));
-	newnode->next=NULL;
-	printf("Enter the data:");
-	scanf("%d",&newnode->data);
-	
-	if(start == NULL)
-	{
-		start=newnode;
-        cur=newnode;
-	}
-	else
-	{
-		cur->next=newnode;
-		cur=newnode;
-	}
-}
-void display()
-{
-	ptr=start;
-	while(ptr->next!=NULL)
-	{
-		printf("%d-->",ptr->data);
-		ptr=ptr->next;
-	}
-	printf("%d-->",ptr->data);
-	printf("NULL\n\n");
-}
+#include"create_display.h"
 int main()
 {
 	int choice;
